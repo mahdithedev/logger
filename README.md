f35290b2b156f0ed5c92da2b1867ef7f7845123a
Mircea-Pavel Anton <mircea.anton99@yahoo.com>
2022-03-13 13:42:11 +0200

    [Release] 1.0.0 (#1)
    * Init npm package
    
    * Import logger code from `pyle-of-mail/scheduler`
    
    * Add doc comments.
    
    * Update README
    
    * Move index.js to root
    
    * Update package.json

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# logger
+# `@pyle-of-mail/logger`
+
+## Description
+
 Minimal and basic logger written in NodeJS.
+
+## Quickstart Guide
+
+### Installing the package
+
+```bash
+npm install @pyle-of-mail/logger
+```
+
+### Setting the log level
+
+There are 4 main types of log messages in this class:
+
+|  Type   |                                       Description                                        |                  Printed                   |
+| :-----: | :--------------------------------------------------------------------------------------: | :----------------------------------------: |
+|  error  |                Used to log problems that require code execution to stop.                 |                   Always                   |
+| warning |             Used to log problems that do not require code execution to stop.             | `LOG_LEVEL` in `["warn", "debug", "info"]` |
+|  info   | Used to print generic informational messages. Informs the user on the actions performed. |     `LOG_LEVEL` in `["debug", "info"]`     |
+|  vars   |  Used to print variables to the console. Mainly intended for development and debugging.  |         `LOG_LEVEL` in `["info"]`          |
+
+### Logging to the console
+
+The logger class will print all your messages to the console by default.
+
+#### Importing the package
+
+```node
+import { Logger } from "@pyle-of-mail/logger";
+```
+
+#### Logging error messages
+
+> Error messages will always be printed to the console, regardless of the `LOG_LEVEL` defined in `.env`.
+
+```javascript
+Logger.logError(
+  "This is an error message. Not only is this printed in red, but it throws an error and stops code execution."
+);
+```
+
+#### Logging warning messages
+
+> Warning messages will only be printed if the `LOG_LEVEL` is set to `warn`, `info` or `debug`.
+
+```javascript
+Logger.logWarn(
+  "This is a warning message, and it should be printed in yellow. This probably means something went wrong, but not wrong enough as to quit execution."
+);
+```
+
+#### Logging info messages
+
+> Info messages will only be printed if the `LOG_LEVEL` is set to `info` or `debug`.
+
+```javascript
+Logger.logInfo("This is an info message, and it should be printed in green.");
+```
+
+#### Printing variables to the screen
+
+> Variables will only be printed if the `LOG_LEVEL` is set to `debug`.
+
+```javascript
+// execute some code
+const response = some_function(some_param);
+
+// log the output
+Logger.logVar(response);
+```
+
+### Logging to a file
+
+Not yet supported, planned for release `1.1.0`.
+
+## License
+
+This code is licensed under the [MIT License](./LICENSE).
---
+++ index.js
@@ -0,0 +1,75 @@
+import "colors";
+
+export class Logger {
+  static log_level;
+
+  /**
+   * Make sure the log level is set.
+   * If it is not defined in the env vars, the default value is "info",
+   * which will print everything but the `logVar()`.
+   */
+  static init() {
+    Logger.log_level = process.env.LOG_LEVEL || "info";
+  }
+
+  static logVar(msg) {
+    // --------------------------------------------------------------------------------------------
+    // Make sure the logger is initialized.
+    // --------------------------------------------------------------------------------------------
+    if (!Logger.log_level) Logger.init();
+
+    if (["info"].includes(Logger.log_level.toLowerCase())) {
+      console.log(msg);
+    }
+  }
+
+  static logInfo(msg) {
+    // --------------------------------------------------------------------------------------------
+    // Make sure the logger is initialized.
+    // --------------------------------------------------------------------------------------------
+    if (!Logger.log_level) Logger.init();
+
+    if (["debug", "info"].includes(Logger.log_level.toLowerCase())) {
+      console.log(msg.green);
+    }
+  }
+
+  /**
+   * Prints the given @msg in red and throws an error.
+   * This will be executed regardless of the LOG_LEVEL set in env vars.
+   *
+   * @param msg: the message to be printed to the console.
+   */
+  static logWarn(msg) {
+    // --------------------------------------------------------------------------------------------
+    // Make sure the logger is initialized.
+    // --------------------------------------------------------------------------------------------
+    if (!Logger.log_level) Logger.init();
+
+    // --------------------------------------------------------------------------------------------
+    // Check the log level. If it's ok, print the message to the console and make it yellow.
+    // --------------------------------------------------------------------------------------------
+    if (["warn", "debug", "info"].includes(Logger.log_level.toLowerCase())) {
+      console.log(msg.yellow);
+    }
+  }
+
+  /**
+   * Prints the given @msg in red and throws an error.
+   * This will be executed regardless of the LOG_LEVEL set in env vars.
+   *
+   * @param msg: the message to be printed to the console.
+   */
+  static logError(msg) {
+    // --------------------------------------------------------------------------------------------
+    // Make sure the logger is initialized.
+    // --------------------------------------------------------------------------------------------
+    if (!Logger.log_level) Logger.init();
+
+    // --------------------------------------------------------------------------------------------
+    // Print the message to the console and throw a new error.
+    // --------------------------------------------------------------------------------------------
+    console.log(msg.red);
+    throw new Error(msg);
+  }
+}
